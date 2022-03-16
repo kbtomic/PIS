2171384d9bf1852c9656f187b98207b576998e54
kbtomic <klara.tomic69@gmail.com>
2022-03-16 19:08:00 +0100

    first commit

--- PIS_prva_vjezba/Program.cs
+++ PIS_prva_vjezba/Program.cs
@@ -7,7 +7,6 @@ namespace PIS_prva_vjezba
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Proba");
         }
     }
 }
