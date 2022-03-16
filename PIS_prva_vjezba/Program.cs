f15653937aa39a2e663d9cf8a3a8bae047799524
z <g>
2022-03-16 19:00:31 +0100

    Add project files.

---
+++ PIS_prva_vjezba/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PIS_prva_vjezba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
